e6fb5ca6ab9b3259c1ef895130a5a3fbca9e8a05
Latif Altay <latifaltay00@gmail.com>
2023-06-30 16:31:28 +0300

    first_Commit

---
+++ lesson1/lesson1/src/test/java/com/lat1faltay/Lesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lat1faltay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
