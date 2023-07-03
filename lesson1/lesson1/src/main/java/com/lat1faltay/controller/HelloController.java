704886dc19ae6633d87592e16e0d524e41b021f2
Latif Altay <latifaltay00@gmail.com>
2023-07-03 15:38:40 +0300

    RequestMappingAnotasyonu

--- lesson1/lesson1/src/main/java/com/lat1faltay/controller/HelloController.java
+++ lesson1/lesson1/src/main/java/com/lat1faltay/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.lat1faltay.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
 
     // localhost:8080/hello
@@ -12,5 +14,5 @@ public class HelloController {
     public String sayHello(){
         return "Hello World!";
     }
-
+ 
 }
