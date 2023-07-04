134d5c33971adbf37f77f354e274101aa247ebe2
Latif Altay <latifaltay00@gmail.com>
2023-07-04 16:18:08 +0300

    Mapping_Anotasyonları

--- lesson1/lesson1/src/main/java/com/lat1faltay/controller/HelloController.java
+++ lesson1/lesson1/src/main/java/com/lat1faltay/controller/HelloController.java
@@ -1,18 +1,34 @@
 package com.lat1faltay.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api") // genellikle class seviyesinde bu anotasyon kullanılır
 public class HelloController {
 
-    // localhost:8080/hello
+    // Mapping Anotasyonları (en çok kullanılan)
+    // GET     : Veri görüntülemek istediğimiz zaman kullanıyoruz
+    // POST    : Veri kaydetmek istediğimiz zaman kullanıyoruz
+    // PUT     : Veri güncellemek için kullanıyoruz
+    // PATCH   : Verinin bir bölümünü güncellemek istediğimiz zaman kullanıyoruz
+    //           Örneğin: Parola güncellemek gibi
+    // DELETE  : Veri silmek istediğimiz zaman kullanıyoruz
 
-    @GetMapping(path = "/hello")
+    // localhost:8080/api/hello
+
+    //@GetMapping(path = "/hello") == alt satır ile aynı anlama geliyor bu satırdaki daha kısa hali
+    @RequestMapping(path = "/hello", method = RequestMethod.GET) // == üst satır ile aynı anlama geliyor sadece daha uzun hali
     public String sayHello(){
         return "Hello World!";
     }
- 
+
+    @PostMapping("/save")
+    public String save(){
+        return "Data Saved";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "Data Deleted!";
+    }
 }
