c95dc4fe43118b1a35d1852f4d0e9be2e96e67e8
Latif Altay <latifaltay00@gmail.com>
2023-07-05 22:28:12 +0300

    @RequestHeader_Anotasyonu

--- lesson6/src/main/java/com/latifaltay/controller/MyController.java
+++ lesson6/src/main/java/com/latifaltay/controller/MyController.java
@@ -1,5 +1,17 @@
 package com.latifaltay.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MyController {
 
+    @GetMapping("/header")
+    public String getHeader(@RequestHeader("My-Header") String myHeader) {
+        return "Your header is : " + myHeader;
+    }
 }
